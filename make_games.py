3f852851ed9e1434da79e151f462219ef0d6d369
Tom <tprice7@gmail.com>
2019-11-13 01:07:40 -0500

    training CNN and FCNN to recognize winner of random games

--- make_games.py
+++ make_games.py
@@ -3,37 +3,63 @@ import numpy as np
 from utils import rb, player_sign
 
 board_size = 5
-num_games = 10
-b = Board(board_size)
+num_games = 50000
+#b = Board(board_size)
 
-positions = np.zeros((num_games, board_size + 1, board_size + 1, 2))
-positions [:, 1:, 0, 0] = 1
-positions [:, 0, 1:, 1] = 1
+positions = np.zeros((num_games, 2, board_size + 1, board_size + 1))
+positions [:, 0, 1:, 0] = 1
+positions [:, 1, 0, 1:] = 1
 winners = np.zeros(num_games)
 
+b = Board(board_size)
+
+# def make_game():
+#     b.refresh()
+#     p = list(np.random.permutation(board_size * board_size))
+#     for i, move in enumerate(p):
+#         wm = b.winning_move(rb[i % 2])
+#         if wm is not None:
+#             b.update(rb[i % 2], wm)
+#             #print(b)
+#             moves = [b.index_to_point(m) for m in p[:i] + [wm]]
+#             return (moves[::2], moves[1::2], b.winner)
+#         b.update(rb[i % 2], move)
+#         #print(b)
+#     b.refresh()
+#     for i, move in enumerate(p):
+#         b.update(rb[i % 2], move)
+#         print(b.winning_move("red"))
+#         print(b.winning_move("blue"))
+#         print(b)
+#         b.print_stuff()
+#         print("----------------------------------------------")
+#         print("----------------------------------------------")
 
 def make_game():
     b.refresh()
     p = list(np.random.permutation(board_size * board_size))
     for i, move in enumerate(p):
-        wm = b.winning_move(rb[i % 2])
-        if wm:
-            b.update(rb[i % 2], wm)
-            moves = [b.index_to_point(m) for m in p[:i] + [wm]]
-            break
         b.update(rb[i % 2], move)
-    return (moves[::2], moves[1::2], b.winner)
-
+        if i % 2 == 1 and b.winner != None:
+            moves = [b.index_to_point(m) for m in p[:i + 1]]
+            return (moves[::2], moves[1::2], b.winner)
+    return make_game()
 
 for i in range(num_games):
+    if i % 1000 == 0:
+        print(i)
     red_moves, blue_moves, winner = make_game()
     for row, column in red_moves:
-        positions[i, row + 1, column + 1, 0] = 1
+        positions[i, 0, row + 1, column + 1] = 1
     for row, column in blue_moves:
-        positions[i, row + 1, column + 1, 1] = 1
-    winners[i] = player_sign[winner]
+        positions[i, 1, row + 1, column + 1] = 1
+    winners[i] = (player_sign[winner] + 1) / 2
 
-for i in range(num_games):
-    print(positions[i, :, :, 0])
-    print(positions[i, :, :, 1])
-    print(winners[i])
+np.savez("training_data.npz", positions = positions, winners = winners)
+
+# for i in range(num_games):
+#     print(positions[i, 0, :, :])
+#     print(positions[i, 1, :, :])
+#     print(np.sum(positions[i, 0, :, :]))
+#     print(np.sum(positions[i, 1, :, :]))
+#     print("Winner: ", winners[i])
--- train.py
+++ train.py
@@ -4,41 +4,71 @@ import torch.optim as optim
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from time import time
 
 training_data = np.load("training_data.npz")
 
-positions = torch.from_numpy(training_data["positions"])
-winners = torch.from_numpy(training_data["winners"])
+positions = torch.tensor(training_data["positions"], dtype = torch.float)
+winners = torch.tensor(training_data["winners"], dtype = torch.float)
 
 dataset = utils.TensorDataset(positions, winners)
 dataloader = utils.DataLoader(dataset)
 
 input_size = np.prod(positions.size()[1:])
 
+class ConvNet(nn.Module):
+    def __init__(self, hidden_layers, breadth):
+        super(ConvNet, self).__init__()
+        self.num_hidden = hidden_layers
+        self.conv = nn.ModuleList([nn.Conv2d(2, breadth, 3)] +
+                                  [nn.Conv2d(breadth, breadth, 3)
+                                   for _ in range(hidden_layers - 1)])
+        self.fc_out = nn.ModuleList([nn.Linear(breadth, 1)] +
+                                    [nn.Linear(breadth, 1, bias=False) for _ in range(hidden_layers - 1)])
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.fc1 = nn.Linear(input_size, 100)
-        self.fc2 = nn.Linear(100, 100)
-        self.fc3 = nn.Linear(100, 100)
-        self.fc4 = nn.Linear(100, 1)
+        self.fc_out[0].bias.data.fill_(0)
+        for i in range(hidden_layers):
+            self.fc_out[i].weight.data.fill_(0)
+
+        self.padding = nn.ConstantPad2d((1, 1, 1, 1), 0)
+
+    def forward(self, x):
+        out = 0
+        for i in range(self.num_hidden):
+            #x = torch.tanh(self.conv[i](self.padding(x)))
+            x = self.conv[i](self.padding(x))
+            x = F.relu(x)
+            #x = x * torch.sigmoid(x)
+            out += self.fc_out[i](torch.sum(x, (2, 3)))
+        return out
+
+
+class FCNet(nn.Module):
+    def __init__(self, breadth):
+        super(FCNet, self).__init__()
+        self.fc1 = nn.Linear(input_size, breadth)
+        self.fc2 = nn.Linear(breadth, breadth)
+        self.fc3 = nn.Linear(breadth, breadth)
+        self.fc4 = nn.Linear(breadth, 1)
 
     def forward(self, x):
         x = x.view(-1, input_size)
-        x = F.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.relu(self.fc3(x))
+        x = torch.tanh(self.fc1(x))
+        #x = F.relu(self.fc2(x))
+        #x = F.relu(self.fc3(x))
         x = self.fc4(x)
         return x
 
-net = Net()
+net = ConvNet(5, 40)
+#net = FCNet(100)
 
 criterion = nn.BCEWithLogitsLoss()
 optimizer = optim.Adam(net.parameters())
 
 correct = 0
 total = 0
+
+start_time = time()
 
 for epoch in range(10):  # loop over the dataset multiple times
 
@@ -53,19 +83,22 @@ for epoch in range(10):  # loop over the dataset multiple times
         # forward + backward + optimize
         outputs = net(inputs)
 
-        total += labels.size[0]
+        total += labels.size()[0]
         correct += ((labels == 1) == (outputs > 0.5)).sum().item()
 
-        loss = criterion(outputs, labels)
+        loss = criterion(outputs.view(-1), labels)
         loss.backward()
         optimizer.step()
 
         # print statistics
         running_loss += loss.item()
         if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(time() - start_time)
+            last_time = time()
             print('[%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 2000))
             running_loss = 0.0
-
-print(correct, total, correct / total)
+            print(correct, total, correct / total)
+            correct = 0
+            total = 0
 
