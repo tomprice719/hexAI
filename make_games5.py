b4b3b0fbed0a7ec715478f2b7ba1b91e93dc41d6
Tom <tprice7@gmail.com>
2019-11-19 00:47:28 -0500

    misc tweaks to make_games5.py

--- make_games5.py
+++ make_games5.py
@@ -4,17 +4,14 @@
 import numpy as np
 
 board_size = 5
-
 batch_size = 5  # number of games to create simultaneously
-
-game_makers = []
-games = []
 games_required = 10000
+game_makers = [GameMaker() for _ in range(batch_size)]
+games = []
 
 while len(games) < games_required:
 
-    game_makers = [g for g in game_makers if not g.finished()]
-    game_makers += [GameMaker() for i in range(batch_size - len(game_makers))]
+    [g.refresh() for g in game_makers if g.finished()]
 
     positions = np.zeros([sum(g.num_positions_required() for g in game_makers), board_size, board_size, 2])
 
@@ -23,11 +20,12 @@ while len(games) < games_required:
         g.fill_positions(positions[position_counter: position_counter + g.num_positions_required()])
         position_counter += g.num_positions_required()
 
-    win_prob = model.predict(positions)
+    win_probs = model.predict(positions)
 
     position_counter = 0
     for g in game_makers:
-        g.update(win_prob[position_counter: position_counter + g.num_positions_required()])
+        g.update(win_probs[position_counter: position_counter + g.num_positions_required()])
+        position_counter += g.num_positions_required()
 
     games += [g.game() for g in game_makers if g.finished()]
 
