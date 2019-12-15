e2a940a74658ea575fa6498bed769f89ffba0dbf
Tom <tprice7@gmail.com>
2019-12-15 03:07:37 -0500

    get_main_model now configurable

--- src/config.py
+++ src/config.py
@@ -1,1 +1,3 @@
 board_size = 9
+main_model_args = {"depth": 18, "breadth": 40, "learning_rate": 0.0001}
+main_model_location = "../data/better_model.h5"
--- src/model.py
+++ src/model.py
@@ -1,16 +1,16 @@
 # predict single winning probability for a board position
 
-from keras.layers import Conv2D, Input, GlobalAveragePooling2D, Dense, Add, Activation
+from keras.layers import Conv2D, Input, GlobalAveragePooling2D, Dense, Add
 from keras.models import Model
 from keras.optimizers import Adam
 from keras.losses import BinaryCrossentropy
 from keras.metrics import BinaryAccuracy
 import itertools
-from config import board_size
+from config import board_size, main_model_args, main_model_location
 from position_utils import input_names
 
 
-def create_model(depth=5, breadth=40, learning_rate=0.001):
+def create_model(depth=5, breadth=20, learning_rate=0.001):
     input_tensors = [Input(shape=(board_size + 1, board_size + 1, 2), name=input_names[k])
                      for k in itertools.product((0, 1), (False, True))]
     out_components = []
@@ -39,36 +39,7 @@ def create_model(depth=5, breadth=40, learning_rate=0.001):
     return model
 
 
-def create_model2(depth=5, breadth=40, breadth2=40, learning_rate=0.001):
-    input_tensors = [Input(shape=(board_size + 1, board_size + 1, 2), name=input_names[k])
-                     for k in itertools.product((0, 1), (False, True))]
-    hidden_components = []
-    tensors = input_tensors
-    pool = GlobalAveragePooling2D()
-
-    for i in range(depth):
-        conv_layer = Conv2D(breadth, 3, padding="same", activation="relu")
-        tensors = list(map(conv_layer, tensors))
-        dense_layer = Dense(breadth2)
-        hidden_components += [dense_layer(pool(t)) for t in tensors[:2]]
-        dense_layer = Dense(breadth2)
-        hidden_components += [dense_layer(pool(t)) for t in tensors[2:]]
-
-    output_tensor = Dense(1, name="winners", kernel_initializer="zeros")(Activation("relu")(Add()(hidden_components)))
-
-    model = Model(input_tensors, [output_tensor])
-
-    optimizer = Adam(lr=learning_rate)
-
-    model.compile(
-        loss=BinaryCrossentropy(from_logits=True),
-        optimizer=optimizer,
-        metrics=[BinaryAccuracy(threshold=0.0)]
-    )
-    return model
-
-
 def get_main_model():
-    model = create_model(depth=18, breadth=40, learning_rate=0.0001)
-    model.load_weights('../data/better_model.h5')
+    model = create_model(**main_model_args)
+    model.load_weights(main_model_location)
     return model
