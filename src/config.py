245982f5575f1512bde99f678c223dcd3bf1a6ba
Tom <tprice7@gmail.com>
2019-12-15 03:09:18 -0500

    use new model

--- src/config.py
+++ src/config.py
@@ -1,3 +1,3 @@
 board_size = 9
 main_model_args = {"depth": 18, "breadth": 40, "learning_rate": 0.0001}
-main_model_location = "../data/better_model.h5"
+main_model_location = "../data/best_model.h5"
