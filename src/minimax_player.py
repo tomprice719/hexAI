6d5f8ee9591388242835ddefc18caf6838fef7f0
Tom <tprice7@gmail.com>
2019-12-16 23:45:35 -0500

    adjust the number of moves we check with 2-ply minimax

--- src/minimax_player.py
+++ src/minimax_player.py
@@ -9,41 +9,54 @@ import yaml
 from string import ascii_uppercase
 
 
-def minimax_move(position, current_player, model, valid_moves):
-    model_input = initialize_model_input(len(valid_moves) * (len(valid_moves) - 1))
+def minimax_move(position, current_player, model, valid_moves, breadth=10):
+    model_input = initialize_model_input(len(valid_moves))
+    fill_model_input(model_input, position, current_player, np.s_[:])
+    update_model_input(model_input,
+                       valid_moves,
+                       current_player,
+                       current_player,
+                       np.s_[:])
+    one_ply_logits = model.predict(model_input)
 
+    minimax_move_indices, minimax_moves = zip(*[(i, move)
+                                                for i, move in enumerate(valid_moves)
+                                                if one_ply_logits[i] >= sorted(one_ply_logits)[-breadth]])
+    assert len(minimax_moves) == breadth
+    moves = [(move1, move2)
+             for move1, move2 in itertools.product(minimax_moves, valid_moves)
+             if move1 != move2]
+
+    model_input = initialize_model_input(len(moves))
     fill_model_input(model_input, position, opposite_player(current_player), np.s_[:])
-
-    moves = [(move1, move2) for move1, move2 in itertools.product(valid_moves, valid_moves) if move1 != move2]
-
     update_model_input(model_input,
                        [move1 for move1, move2 in moves],
                        current_player,
                        opposite_player(current_player),
                        np.s_[:])
-
     update_model_input(model_input,
                        [move2 for move1, move2 in moves],
                        opposite_player(current_player),
                        opposite_player(current_player),
                        np.s_[:])
+    two_ply_logits = model.predict(model_input)
 
-    print([len(x) for x in model_input.values()])
-    win_logits = model.predict(model_input)
+    minimax_logits = []
+    for i in range(len(minimax_moves)):
+        minimax_logits.append(-max(two_ply_logits[:len(valid_moves) - 1]))
+        two_ply_logits = two_ply_logits[len(valid_moves) - 1:]
+    assert len(two_ply_logits) == 0
+    assert len(minimax_logits) == len(minimax_moves)
 
-    # get best move via minimax
+    for j, i in enumerate(minimax_move_indices):
+        one_ply_logits[i] = minimax_logits[j]
 
-    maximums = []
-    for i in range(len(valid_moves)):
-        maximums.append(max(win_logits[:len(valid_moves) - 1]))
-        win_logits = win_logits[len(valid_moves) - 1:]
-    assert len(win_logits) == 0
+    best_response_index, best_response = max(enumerate(valid_moves), key=lambda x: one_ply_logits[x[0]])
+    assert one_ply_logits[best_response_index] == max(one_ply_logits)
 
-    print(min(maximums))
+    print(best_response in minimax_moves)
 
-    best_response = valid_moves[min(range(len(valid_moves)), key=lambda x: maximums[x])]
-
-    return best_response, -min(maximums)
+    return best_response, one_ply_logits[best_response_index]
 
 def get_move(valid_moves):
     while True:
